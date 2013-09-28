0045ac39bcd6021c103036a6635b5c81b5c2ceec
Shane Celis <shane@gnufoo.org>
2013-09-27 22:54:56 -0400

    It works better with leap motion now.

--- leapmotion-event.scm
+++ leapmotion-event.scm
@@ -2,70 +2,89 @@
   #:use-module (LeapGuile)
   #:use-module ((LeapGuile-primitive) :renamer (symbol-prefix-proc 'primitive:))
   #:use-module (oop goops)
-  #:use-module ((srfi srfi-1) #:select (fold)) ;; fold
+  #:use-module ((srfi srfi-1) #:select (fold iota)) ;; fold iota
   #:use-module ((emacsy emacsy) #:hide (state position))
   #:use-module (ice-9 match)
   #:use-module (ice-9 regex)
   #:use-module (logging)
+  #:use-module ((guile-user) #:select (get-modifier-key-list))
   #:export (leapmotion-event-init
+            make-leapmotion-poller
             <leapmotion-event>
-            lme:frame))
+            <leapmotion-hand-event>
+            <leapmotion-pointable-event>
+            le:hand
+            le:pointable
+            le:frame
+            le:event-type))
 
-(define-class <leapmotion-event> (<key-event>)
-  (lme:frame #:getter lme:frame #:init-keyword #:lme:frame))
+(define-class <leapmotion-event> (<modifier-key-event>)
+  (frame #:getter le:frame #:init-keyword #:le:frame)
+  (event-type #:getter le:event-type #:init-keyword #:le:event-type #:init-value 'frame))
+
+(define-class <leapmotion-hand-event> (<leapmotion-event>)
+  (hand #:getter le:hand #:init-keyword #:le:hand))
+
+(define-class <leapmotion-pointable-event> (<leapmotion-hand-event>)
+  (pointable #:getter le:pointable #:init-keyword #:le:pointable))
+
 
 #|
 
 How do we describe leap motion events?
 
 lm-circle
-lm-finger
+lm-tap
+lm-swipe
+lm-screen-tap
+lm-finger-{1,2,3...}
+lm-hand-
+lm-hand-1-finger-{1,2,3}
+lm-oldest-finger
 lm-pointer
 lm-frame
 
 |#
 
 ;; Turn this into debug output.
-(define (format port . args)
-  (apply mylog "leapmotion-event" pri-trace args))
 
 (define-method (event->kbd (event <leapmotion-event>))
-  (let ((mods (map string (map modifier-symbol->char (modifier-keys event)))))
-    (string-join `(,@mods "lm-frame") "-")))
+  (let ((mods (next-method)))
+    (format #f "~a~a~a" mods "lm-" (le:event-type event))))
+
+;; Wow. I just spent a ton of time debugging because I did this. Don't. Stop!
+(define (my-format port . args)
+  (apply mylog "leapmotion-event" pri-trace args))
 
 (define-kbd-converter (kbd-entry->leapmotion-event kbd-entry)
-    (let ((regex "^(([ACHMsS]-)*)lm-([^ ]*)$"))
-    (define (get-modifier-keys match)
-      (let* ((str (match:substring match 1)))
-        (if str
-            (map modifier-char->symbol 
-                 (filter (lambda (x) (not (char=? x #\-))) (string->list str)))
-            '())))
-    (let ((match (string-match regex kbd-entry)))
-      (if match
-          (let* ((mod-keys (get-modifier-keys match)))
-            (make <leapmotion-event> #:modifier-keys mod-keys))
-          #f))))
+  (match (strip-off-modifier-keys kbd-entry)
+    ((mod-keys kbd-entry)
+     (let* ((regex "^lm-([^ ]*)$")
+            (rmatch (string-match regex kbd-entry)))
+       (if rmatch
+           (make <leapmotion-event> #:modifier-keys mod-keys
+                 #:le:event-type (string->symbol (match:substring rmatch 1)))
+           #f)))))
 
 (define-class <emacsy-listener> (<SwigDirector-Listener>)
   ;; XXX this plumbing should be hidden
   #:new-function primitive:new-SwigDirector-Listener)
 
 (define-method (onInit (self <emacsy-listener>) (controller <Controller>))
-  (format #t "Initialized~%"))
+  (my-format #t "Initialized~%"))
 
 (define-method (onConnect (self <emacsy-listener>) (controller <Controller>))
-  (format #t "Connected~%")
+  (my-format #t "Connected~%")
   (enableGesture controller (primitive:Gesture-TYPE-CIRCLE))
   (enableGesture controller (primitive:Gesture-TYPE-KEY-TAP))
   (enableGesture controller (primitive:Gesture-TYPE-SCREEN-TAP))
   (enableGesture controller (primitive:Gesture-TYPE-SWIPE)))
 
 (define-method (onDisconnect (self <emacsy-listener>) (controller <Controller>))
-  (format #t "Disconnected~%"))
+  (my-format #t "Disconnected~%"))
 
 (define-method (onExit (self <emacsy-listener>) (controller <Controller>))
-  (format #t "Exited~%"))
+  (my-format #t "Exited~%"))
 
 (define (*List->list List-object)
   "Convert a HandList, PointableList, etc. to a regular scheme list."
@@ -80,11 +99,18 @@ lm-frame
   ;; Get the most recent frame and report some basic information
   (let ((frame-proc frame)
         (frame (frame controller)))
-    (emacsy-event (make <leapmotion-event> #:lme:frame frame
-                        #:modifier-keys '(shift)
-                        #:command-char #\A ;; bullshit
-                        ))
-   (format #t "Frame id: ~a, timestamp: ~a, hands: ~a, fingers: ~a, tools: ~a, gestures: ~a~%"
+    (emacsy-event (make <leapmotion-event> 
+                    #:le:frame frame 
+                    #:discrete-event? #f
+                    #:modifier-keys (get-modifier-key-list)))
+    (for-each (lambda (finger index)
+                (emacsy-event (make <leapmotion-pointable-event> 
+                    #:le:frame frame 
+                    #:le:pointable (string->symbol (format #f "finger-~a" index))
+                    #:discrete-event? #f
+                    #:modifier-keys (get-modifier-key-list))))
+              (*List->list fingers) (iota (count (fingers frame)) 1))
+   (my-format #t "Frame id: ~a, timestamp: ~a, hands: ~a, fingers: ~a, tools: ~a, gestures: ~a~%"
            (id frame)
            (timestamp frame)
            (count (hands frame))
@@ -102,10 +128,10 @@ lm-frame
            (Leap+ avg-pos avg-pos)
            (set! avg-pos (fold Leap+ avg-pos (map tipPosition (*List->list fingers))))
            (set! avg-pos (Leap/ avg-pos (count fingers)))
-           (format #t "Hand has ~a fingers, average finger tip position ~a~%" (count fingers) avg-pos)))
+           (my-format #t "Hand has ~a fingers, average finger tip position ~a~%" (count fingers) avg-pos)))
        
        ;; Get the hand's sphere radius and palm position
-       (format #t "Hand sphere radius: ~a mm, palm position: ~a~%"
+       (my-format #t "Hand sphere radius: ~a mm, palm position: ~a~%"
                (sphereRadius hand)
                (palmPosition hand))
        
@@ -142,7 +168,7 @@ lm-frame
                                                     (id circle))))))
                      (* (- (progress circle) (progress previousUpdate))
                         2 pi)))))
-           (format 
+           (my-format 
             #t
             "Circle id: ~a, state: ~a, progress: ~a, radius: ~a, angle ~a ~a ~%"            (id circle)
             (state circle)
@@ -155,7 +181,7 @@ lm-frame
          ;; Swipe Gesture
          ((= (type a-gesture) (primitive:Gesture-TYPE-SWIPE))
           (let ((swipe (make <SwipeGesture> #:args (list a-gesture))))
-            (format 
+            (my-format 
              #t
              "Swipe id: ~a, state: ~a, direction: ~a, speed ~a~%"
              (id swipe)
@@ -165,7 +191,7 @@ lm-frame
          ;; Key Tap Gesture
          ((= (type a-gesture) (primitive:Gesture-TYPE-KEY-TAP))
           (let ((tap (make <KeyTapGesture> #:args (list a-gesture))))
-            (format
+            (my-format
              #t
              "Key Tap id: ~a, state: ~a, position: ~a, direction: ~a"
              (id tap)
@@ -175,7 +201,7 @@ lm-frame
          ;; Screen Tap Gesture
          ((= (type a-gesture) (primitive:Gesture-TYPE-SCREEN-TAP))
           (let ((tap (make <ScreenTapGesture> #:args (list a-gesture))))
-            (format
+            (my-format
              #t
              "Screen Tap id: ~a, state: ~a, position: ~a, direction: ~a"
              (id tap)
@@ -186,10 +212,10 @@ lm-frame
 
 
 (define-method (onFocusGained (self <emacsy-listener>) (controller <Controller>))
-  (format #t "Focus Gained~%"))
+  (my-format #t "Focus Gained~%"))
 
 (define-method (onFocusLost (self <emacsy-listener>) (controller <Controller>))
-  (format #t "Focus Lost~%"))
+  (my-format #t "Focus Lost~%"))
 
 (define listener #f)
 (define controller #f)
@@ -202,4 +228,33 @@ lm-frame
   (setSelf listener listener)
   ;; Have the sample listener receive events from the controller
   (addListener controller listener))
+
+(define (make-leapmotion-poller)
+  (set! controller (make <Controller>))
+  (lambda ()
+    (let ((the-frame (frame controller)))
+     (emacsy-event (make <leapmotion-event> 
+                     #:le:frame the-frame
+                     #:discrete-event? #f
+                     #:modifier-keys (get-modifier-key-list)))
+     (apply for-each 
+      (lambda (the-hand hand-index)
+        (emacsy-event (make <leapmotion-hand-event> 
+                                    #:le:frame the-frame 
+                                    #:le:hand the-hand
+                                    #:le:event-type (string->symbol (format #f "hand-~a" hand-index))
+                                    #:discrete-event? #f
+                                    #:modifier-keys (get-modifier-key-list)))
+        (for-each (lambda (the-finger index)
+                    (emacsy-event (make <leapmotion-pointable-event> 
+                                    #:le:frame the-frame 
+                                    #:le:hand the-hand
+                                    #:le:event-type (string->symbol (format #f "hand-~a-finger-~a" hand-index index))
+                                    #:le:pointable the-finger
+                                    #:discrete-event? #f
+                                    #:modifier-keys (get-modifier-key-list))))
+                  (*List->list (fingers the-hand)) (iota (count (fingers the-frame)) 1)))
+      (let ((my-hands (hands the-frame)))
+        (list (*List->list my-hands) 
+              (iota (count my-hands) 1)))))))
 
